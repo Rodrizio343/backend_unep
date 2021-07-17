0a75b4f7c52d8f448a64949bb6a62e337fb623f1
Rodrigo Martinez <rodrigo.martinez@webexport.com.ar>
2021-07-17 16:19:47 -0300

    Plugin graphql

--- config/plugins.js
+++ config/plugins.js
@@ -10,4 +10,15 @@ module.exports = ({ env }) => ({
             },
         },
     },
+
+    graphql: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+    },
 });
